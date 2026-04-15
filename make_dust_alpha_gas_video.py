9deba0a7800f32ebd185d952f06b58e542443353
Eric Moseley <ericm628@gmail.com>
2026-04-15 12:16:14 -0700

    cb swapped side

--- make_dust_alpha_gas_video.py
+++ make_dust_alpha_gas_video.py
@@ -115,6 +115,9 @@ def render_frame(
         orientation="vertical",
     )
     cb_d.set_label(DUST_CB_LABEL)
+    # Left colorbar: put ticks on outer (left) side, axis label on inner (right) side
+    cb_d.ax.yaxis.set_ticks_position("left")
+    cb_d.ax.yaxis.set_label_position("right")
 
     cb_g = fig.colorbar(
         ScalarMappable(norm=norm_g, cmap=GAS_CMAP),
